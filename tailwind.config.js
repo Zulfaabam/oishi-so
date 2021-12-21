45ca8faf4dfdf3b5ed4b9af5dc6144bdf7678019
Zulfaabam <63113235+Zulfaabam@users.noreply.github.com>
2021-12-21 21:53:58 +0700

    first commit oishi-so

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    fontFamily: {
+      sans: ['DM Sans', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        primary: '#F7931E',
+      },
+    },
+  },
+  plugins: [],
+}
