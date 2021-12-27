b2b13e14c50bdcda5d03f5dbe97b9d6c26e79a9e
Zulfaabam <63113235+Zulfaabam@users.noreply.github.com>
2021-12-27 16:25:25 +0700

    edit footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,14 +9,14 @@ import { NavLink } from 'react-router-dom'
 
 export default function Footer() {
   return (
-    <footer className="bg-dark-90 py-12 space-y-6">
+    <footer className="bg-dark-90 pt-12 pb-8 space-y-6">
       <NavLink to="/" className="flex justify-center items-center gap-3">
         <img src="/logo-white.png" alt="website logo" />
         <h1 className="text-2xl md:text-3xl font-bold italic text-white">
           Oishi-so
         </h1>
       </NavLink>
-      <div className="text-white text-center md:text-2xl">
+      <div className="text-white text-center md:text-xl">
         <p>The only thing we are serious about is food.</p>
         <p>Contact us on</p>
       </div>
@@ -26,6 +26,17 @@ export default function Footer() {
         <FaTwitter className="text-2xl md:text-4xl cursor-pointer" />
         <FaFacebookSquare className="text-2xl md:text-4xl cursor-pointer" />
       </div>
+      <p className="text-white text-center md:text-lg">
+        Coded by{' '}
+        <a
+          href="https://abams.vercel.app/"
+          target="_blank"
+          rel="noreferrer"
+          className="underline hover:text-primary transition-all"
+        >
+          Abam
+        </a>
+      </p>
     </footer>
   )
 }
